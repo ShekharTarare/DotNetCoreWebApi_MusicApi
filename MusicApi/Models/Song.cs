e70ff9a8e4beaf1ad01ea5a1a99dee577bde86f8
Shekhar Tarare <shekhartarare@gmail.com>
2022-02-07 02:36:47 +0530

    Added the validations

--- MusicApi/Models/Song.cs
+++ MusicApi/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace MusicApi.Models
     public class Song
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Title cannot be null or empty!")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Language cannot be null or empty!")]
         public string Language { get; set; }
+        [Required(ErrorMessage = "Kindly provide the song duration!")]
         public string Duration { get; set; }
     }
 }
